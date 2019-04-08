cd97a712c28c3247ae7b1858108457442924dee3
c4_angel <20829444@qq.com>
2019-04-08 08:14:22 +0800

    Update patch.bat

--- patch.bat
+++ patch.bat
@@ -6,7 +6,7 @@ cls
 
 weidu.exe CDTWEAKS/SETUP-CDTWEAKS.TP2 --force-install 4140 --language 9
 
-weidu.exe c4Config_IA/c4Config_IA.TP2 --force-install-list 1 2
+weidu.exe c4Config_IA/c4Config_IA.TP2 --force-install-list 1
 
 start https://github.com/Sebastian-c4/mods_collection/blob/master/BG2EE_IA_c4Customize.md
 
